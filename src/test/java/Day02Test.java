510bdf26b2f7547f59af900ea6ed5278d19298fc
Greg King <king.greg@gmail.com>
2019-01-18 17:07:25 -0500

    Days 2-3

---
+++ src/test/java/Day02Test.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class Day02Test {
+
+	@Test
+	public void test1a() throws FileNotFoundException {
+	    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day02/test1a.txt").getPath());
+	    final Day02 day02 = new Day02(fileReader);
+	    Assertions.assertThat(day02.getCode()).isEqualTo(1985);
+	}
+	
+	@Test
+	public void testSolution1() throws FileNotFoundException {
+	    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day02/input.txt").getPath());
+	    final Day02 day02 = new Day02(fileReader);
+	    Assertions.assertThat(day02.getCode()).isEqualTo(35749);
+	}
+	
+	@Test
+	public void test2a() throws FileNotFoundException {
+	    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day02/test1a.txt").getPath());
+	    final Day02 day02 = new Day02(fileReader, true);
+	    Assertions.assertThat(day02.getCode()).isEqualTo(0x5DB3);
+	}
+	
+	@Test
+	public void testSolution2() throws FileNotFoundException {
+	    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day02/input.txt").getPath());
+	    final Day02 day02 = new Day02(fileReader, true);
+	    Assertions.assertThat(day02.getCode()).isEqualTo(0x9365C);
+	}
+
+}
---
+++ src/test/java/Day03Test.java
@@ -0,0 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class Day03Test {
+
+	@Test
+	public void testSolution1() throws FileNotFoundException {
+	    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day03/input.txt").getPath());
+	    final Day03 day03 = new Day03(fileReader);
+	    Assertions.assertThat(day03.validTriangles()).isEqualTo(862);
+	}
+	
+	@Test
+	public void testSolution2() throws FileNotFoundException {
+	    final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("Day03/input.txt").getPath());
+	    final Day03 day03 = new Day03(fileReader, true);
+	    Assertions.assertThat(day03.validTriangles()).isEqualTo(1577);
+	}
+
+}
