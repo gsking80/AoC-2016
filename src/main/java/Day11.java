de1f5e66f657a81c29ae598cf6f38619796846a8
Greg King <king.greg@gmail.com>
2019-01-28 17:11:24 -0500

    Day 11 cont

--- src/main/java/Day11.java
+++ src/main/java/Day11.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -33,12 +32,27 @@ public class Day11 {
 
     while(!priorityQueue.isEmpty()) {
       current = priorityQueue.remove();
+      System.out.println("Estimated moves remaining - " + current.getEstimatedRemainingMoves() + " --- depth " + priorityQueue.size());
 
       if(!visited.contains(current)) {
         visited.add(current);
         if (current.getEstimatedRemainingMoves() == 0) {
           return distances.get(current);
         }
+		// iterate over the neighbors
+		for (State neighbor : current.possibleNextStates()) {
+			if (!visited.contains(neighbor)) {
+				// new State
+				int predictedTimeFromNeighbor = neighbor.getEstimatedRemainingMoves();
+				int totalTime = current.getMoves() + 1
+						+ predictedTimeFromNeighbor;
+				if (distances.get(neighbor) == null || totalTime < distances.get(neighbor)) {
+					distances.put(neighbor, totalTime);
+					neighbor.setMoves(current.getMoves() + 1);
+					priorityQueue.add(neighbor);
+				}
+			}
+		}
       }
 
 
@@ -60,6 +74,43 @@ public class Day11 {
   }
 
   class State {
+	@Override
+	public String toString() {
+		return "State [moves=" + moves + ", elevatorFloor=" + elevatorFloor + ", elements=" + elements + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + getOuterType().hashCode();
+		result = prime * result + ((elements == null) ? 0 : elements.hashCode());
+		result = prime * result + elevatorFloor;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (!getOuterType().equals(other.getOuterType()))
+			return false;
+		if (elements == null) {
+			if (other.elements != null)
+				return false;
+		} else if (!elements.equals(other.elements))
+			return false;
+		if (elevatorFloor != other.elevatorFloor)
+			return false;
+		return true;
+	}
+
+	private int moves;
     private int elevatorFloor;
     private Set<Element> elements;
 
@@ -68,7 +119,15 @@ public class Day11 {
       this.elements = elements;
     }
 
-    public int getEstimatedTotalMoves(){
+    public int getMoves() {
+		return moves;
+	}
+
+	public void setMoves(int moves) {
+		this.moves = moves;
+	}
+
+	public int getEstimatedTotalMoves(){
       return distances.get(this) + getEstimatedRemainingMoves();
     }
 
@@ -78,30 +137,216 @@ public class Day11 {
         remaining += (4 - element.getGeneratorFloor());
         remaining += (4 - element.getMicrochipFloor());
       }
-      return remaining / 2;
+//      return (remaining / 2) + remaining % 2;
+      return remaining;
     }
 
     public List<State> possibleNextStates() {
       final List<State> next = new ArrayList<>();
       final List<Element> generatorsOnFloor = elements.stream().filter(p -> p.getGeneratorFloor() == elevatorFloor).collect(
           Collectors.toList());
-      final List<Element> microChipsOnFloor = elements.stream().filter(p -> p.getMicrochipFloor() == elevatorFloor).collect(
+      final List<Element> microchipsOnFloor = elements.stream().filter(p -> p.getMicrochipFloor() == elevatorFloor).collect(
           Collectors.toList());
       if (elevatorFloor != 1) {
         //move down
         for(final Element generator: generatorsOnFloor) {
-
+        	final Set<Element> newElements = new HashSet<>();
+        	newElements.add(new Element(generator.name, generator.getGeneratorFloor() - 1, generator.getMicrochipFloor()));
+        	for(final Element other: elements) {
+        		if (!other.name.equals(generator.name)) {
+        			newElements.add(new Element(other));
+        		}
+        	}
+        	final State newState = new State(elevatorFloor - 1, newElements);
+        	if (newState.isValid()) {
+        		next.add(newState);
+        	}
+        	if (generator.microchipFloor == generator.generatorFloor) {
+            	final Set<Element> newElements2 = new HashSet<>();
+            	newElements2.add(new Element(generator.name, generator.getGeneratorFloor() - 1, generator.getMicrochipFloor() - 1));
+            	for(final Element other: elements) {
+            		if (!other.name.equals(generator.name)) {
+            			newElements2.add(new Element(other));
+            		}
+            	}
+            	final State newState2 = new State(elevatorFloor - 1, newElements2);
+            	if (newState2.isValid()) {
+            		next.add(newState2);
+            	}
+        	}
+        	for(final Element generator2: generatorsOnFloor) {
+            	final Set<Element> newElements2 = new HashSet<>();
+            	newElements2.add(new Element(generator.name, generator.getGeneratorFloor() - 1, generator.getMicrochipFloor()));
+            	newElements2.add(new Element(generator2.name, generator2.getGeneratorFloor() - 1, generator2.getMicrochipFloor()));
+            	for(final Element other: elements) {
+            		if (!other.name.equals(generator.name) && !other.name.equals(generator2.name)) {
+            			newElements2.add(new Element(other));
+            		}
+            	}
+            	final State newState2 = new State(elevatorFloor - 1, newElements2);
+            	if (newState2.isValid()) {
+            		next.add(newState2);
+            	}
+        	}
+        }
+        for(final Element microchip: microchipsOnFloor) {
+        	final Set<Element> newElements = new HashSet<>();
+        	newElements.add(new Element(microchip.name, microchip.getGeneratorFloor(), microchip.getMicrochipFloor() - 1));
+        	for(final Element other: elements) {
+        		if (!other.name.equals(microchip.name)) {
+        			newElements.add(new Element(other));
+        		}
+        	}
+        	final State newState = new State(elevatorFloor - 1, newElements);
+        	if (newState.isValid()) {
+        		next.add(newState);
+        	}
+        	for(final Element microchip2: generatorsOnFloor) {
+            	final Set<Element> newElements2 = new HashSet<>();
+            	newElements2.add(new Element(microchip.name, microchip.getGeneratorFloor(), microchip.getMicrochipFloor() - 1));
+            	newElements2.add(new Element(microchip2.name, microchip2.getGeneratorFloor(), microchip2.getMicrochipFloor() - 1));
+            	for(final Element other: elements) {
+            		if (!other.name.equals(microchip.name) && !other.name.equals(microchip2.name)) {
+            			newElements2.add(new Element(other));
+            		}
+            	}
+            	final State newState2 = new State(elevatorFloor - 1, newElements2);
+            	if (newState2.isValid()) {
+            		next.add(newState2);
+            	}
+        	}
         }
       }
       if (elevatorFloor != 4) {
         //move up
+          for(final Element generator: generatorsOnFloor) {
+          	final Set<Element> newElements = new HashSet<>();
+          	newElements.add(new Element(generator.name, generator.getGeneratorFloor() + 1, generator.getMicrochipFloor()));
+          	for(final Element other: elements) {
+          		if (!other.name.equals(generator.name)) {
+          			newElements.add(new Element(other));
+          		}
+          	}
+          	final State newState = new State(elevatorFloor + 1, newElements);
+          	if (newState.isValid()) {
+          		next.add(newState);
+          	}
+          	if (generator.microchipFloor == generator.generatorFloor) {
+              	final Set<Element> newElements2 = new HashSet<>();
+              	newElements2.add(new Element(generator.name, generator.getGeneratorFloor() + 1, generator.getMicrochipFloor() + 1));
+              	for(final Element other: elements) {
+              		if (!other.name.equals(generator.name)) {
+              			newElements2.add(new Element(other));
+              		}
+              	}
+              	final State newState2 = new State(elevatorFloor + 1, newElements2);
+              	if (newState2.isValid()) {
+              		next.add(newState2);
+              	}
+          	}
+          	for(final Element generator2: generatorsOnFloor) {
+              	final Set<Element> newElements2 = new HashSet<>();
+              	newElements2.add(new Element(generator.name, generator.getGeneratorFloor() + 1, generator.getMicrochipFloor()));
+              	newElements2.add(new Element(generator2.name, generator2.getGeneratorFloor() + 1, generator2.getMicrochipFloor()));
+              	for(final Element other: elements) {
+              		if (!other.name.equals(generator.name) && !other.name.equals(generator2.name)) {
+              			newElements2.add(new Element(other));
+              		}
+              	}
+              	final State newState2 = new State(elevatorFloor + 1, newElements2);
+              	if (newState2.isValid()) {
+              		next.add(newState2);
+              	}
+          	}
+          }
+          for(final Element microchip: microchipsOnFloor) {
+          	final Set<Element> newElements = new HashSet<>();
+          	newElements.add(new Element(microchip.name, microchip.getGeneratorFloor(), microchip.getMicrochipFloor() + 1));
+          	for(final Element other: elements) {
+          		if (!other.name.equals(microchip.name)) {
+          			newElements.add(new Element(other));
+          		}
+          	}
+          	final State newState = new State(elevatorFloor + 1, newElements);
+          	if (newState.isValid()) {
+          		next.add(newState);
+          	}
+          	for(final Element microchip2: generatorsOnFloor) {
+              	final Set<Element> newElements2 = new HashSet<>();
+              	newElements2.add(new Element(microchip.name, microchip.getGeneratorFloor(), microchip.getMicrochipFloor() + 1));
+              	newElements2.add(new Element(microchip2.name, microchip2.getGeneratorFloor(), microchip2.getMicrochipFloor() + 1));
+              	for(final Element other: elements) {
+              		if (!other.name.equals(microchip.name) && !other.name.equals(microchip2.name)) {
+              			newElements2.add(new Element(other));
+              		}
+              	}
+              	final State newState2 = new State(elevatorFloor + 1, newElements2);
+              	if (newState2.isValid()) {
+              		next.add(newState2);
+              	}
+          	}
+          }
       }
       return next;
     }
+    
+    public boolean isValid() {
+    	for (final Element element1: elements) {
+    		if (element1.generatorFloor != element1.microchipFloor) {
+    			for (final Element element2: elements) {
+    				if (element1.microchipFloor == element2.generatorFloor) {
+    					return false;
+    				}
+    			}
+    		}
+    	}
+    	return true;
+    }
+
+	private Day11 getOuterType() {
+		return Day11.this;
+	}
   }
 
   static class Element {
-    private String name;
+    @Override
+	public String toString() {
+		return "Element [name=" + name + ", generatorFloor=" + generatorFloor + ", microchipFloor=" + microchipFloor
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + generatorFloor;
+		result = prime * result + microchipFloor;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Element other = (Element) obj;
+		if (generatorFloor != other.generatorFloor)
+			return false;
+		if (microchipFloor != other.microchipFloor)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	private String name;
     private int generatorFloor;
     private int microchipFloor;
 
@@ -109,6 +354,12 @@ public class Day11 {
       this.name = name;
       this.generatorFloor = generatorFloor;
       this.microchipFloor = microchipFloor;
+    }
+    
+    Element(final Element copy) {
+    	this.name = copy.name;
+    	this.generatorFloor = copy.generatorFloor;
+    	this.microchipFloor = copy.microchipFloor;
     }
 
     public int getGeneratorFloor() {
--- src/test/java/Day11Test.java
+++ src/test/java/Day11Test.java
@@ -3,10 +3,17 @@ import org.junit.Test;
 
 public class Day11Test {
 
-  @Test
-  public void test1a() {
-    final Day11 day11 = new Day11(new Day11.Element("H", 2, 1), new Day11.Element("Li", 3, 1));
-    Assertions.assertThat(day11.minMoves()).isEqualTo(11);
-  }
+	@Test
+	public void test1a() {
+		final Day11 day11 = new Day11(new Day11.Element("H", 2, 1), new Day11.Element("Li", 3, 1));
+		Assertions.assertThat(day11.minMoves()).isEqualTo(11);
+	}
+
+	@Test
+	public void testSolution1() {
+		final Day11 day11 = new Day11(new Day11.Element("Pm", 1, 1), new Day11.Element("Co", 2, 3),
+				new Day11.Element("Cm", 2, 3), new Day11.Element("Ru", 2, 3), new Day11.Element("Pu", 2, 3));
+		Assertions.assertThat(day11.minMoves()).isEqualTo(27);  // Not 27
+	}
 
 }
